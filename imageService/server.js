728ada26464dc9a6c0e3405ed59837111761cbc0
Alexander Bakalov <59098820+Perrytheplaty43@users.noreply.github.com>
2023-02-18 21:17:41 -0800

    Adding full backend integration
    Making certain variables env variables

--- imageService/server.js
+++ imageService/server.js
@@ -4,17 +4,20 @@ import Tesseract from 'tesseract.js'
 import urlParser from 'url'
 
 async function extractTextFromImage(url) {
-    try {
-        console.log(url);
-        const response = await fetch(url).catch(error => console.log("catch", error));
-        console.log('hi');
-        const buffer = await response.arrayBuffer();
-        const image = await Tesseract.recognize(buffer, 'eng');
-        return image.data.text;
-    } catch (error) {
-        console.log(error);
-        return "Error occurred while extracting text";
-    }
+	try {
+		if (url.endsWith('jpg') || url.endsWith('png') || url.endsWith('bmp') || url.endsWith('pbm')) {
+			const response = await fetch(url).catch(error => console.log("catch", error));
+			console.log(url, "--------------------------")
+			const buffer = await response.arrayBuffer();
+			let image = await Tesseract.recognize(buffer, 'eng', { errorHandler: m => console.log(m) })
+			return image.data.text;
+		} else {
+			return "_"
+		}
+	} catch (error) {
+		console.log(error);
+		return "Error occurred while extracting text";
+	}
 }
 
 http.createServer((request, response) => {
@@ -22,9 +25,12 @@ http.createServer((request, response) => {
 	const url_parts = urlParser.parse(request.url);
 	if (method == 'GET' && url_parts.query) {
 		let imageUrl = url_parts.query.substring(4);
+		console.log(1)
 		if (imageUrl) {
+			console.log(2)
 			extractTextFromImage(imageUrl)
 				.then(text => {
+					console.log(text)
 					response.writeHead(200, { 'Content-Type': 'text/plain' });
 					response.write(text);
 					response.end();
