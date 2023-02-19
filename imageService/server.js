9ef2d1404c29858fa3874839c1ab38f27663ed56
Alexander Bakalov <59098820+Perrytheplaty43@users.noreply.github.com>
2023-02-18 19:41:58 -0800

    Adding relevant npm dependencies

--- imageService/server.js
+++ imageService/server.js
@@ -1,30 +1,40 @@
-/**
- * 
- */
-const http = require('http');
-const fs = require('fs');
-const url = require('url');
-const { extractTextFromImage } = require('./imageProcesser');
+import http from 'http'
+import fetch from 'node-fetch'
+import Tesseract from 'tesseract.js'
+
+async function extractTextFromImage(url) {
+    try {
+        console.log(url);
+        const response = await fetch(url).catch(error => console.log("catch", error));
+        console.log('hi');
+        const buffer = await response.buffer();
+        const image = await Tesseract.recognize(buffer, 'eng');
+        return image.data.text;
+    } catch (error) {
+        console.log(error);
+        return "Error occurred while extracting text";
+    }
+}
 
 http.createServer((request, response) => {
-	const{method, url} = request
+	const { method, url } = request
 	const url_parts = require('url').parse(request.url);
-	if (method == 'GET' && url_parts.query){
+	if (method == 'GET' && url_parts.query) {
 		let imageUrl = url_parts.query.substring(4);
-		if (imageUrl){
+		if (imageUrl) {
 			console.log('works');
-			 extractTextFromImage(imageUrl)
-			    .then(text => {
-			      response.writeHead(200, { 'Content-Type': 'text/plain' });
-			      response.write(text);
-			      response.end();
-			    })
-			    .catch(error => {
-			      console.error(error);
-			      response.writeHead(500, { 'Content-Type': 'text/plain' });
-			      response.write('Error occurred while extracting text');
-			      response.end();
-    			});
+			extractTextFromImage(imageUrl)
+				.then(text => {
+					response.writeHead(200, { 'Content-Type': 'text/plain' });
+					response.write(text);
+					response.end();
+				})
+				.catch(error => {
+					console.error(error);
+					response.writeHead(500, { 'Content-Type': 'text/plain' });
+					response.write('Error occurred while extracting text');
+					response.end();
+				});
 		}
 	}
-}).listen(80);
+}).listen(81);
